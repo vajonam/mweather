9a3b583c097e0ffdcbccdbc2576bb1a9d1908e1d
Jared Biehler <jaredbcoding@gmail.com>
2014-04-29 22:14:21 -0400

    cleanup up gh-pages

--- src/config.h
+++
@@ -1,8 +0,0 @@
-// "c" for temperature in Celsius
-// "f" for temperature in Fahrenheit
-#define UNIT_SYSTEM "f"
-#define DEFAULT_DEBUG_MODE false
-#define DEFAULT_WEATHER_SERVICE "yahoo"
-
-#define KEY_DEBUG_MODE 0
-#define KEY_WEATHER_SERVICE 1
