2069bcf5cac52f27cc551b5f4e5d01f6c13bec54
Manojav Sridhar <manojav@manojav.com>
2014-08-22 12:30:42 -0400

    update repository name

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,7 +1,7 @@
 var SERVICE_OPEN_WEATHER  = "open";
 var SERVICE_YAHOO_WEATHER = "yahoo";
 var EXTERNAL_DEBUG_URL    = '';
-var CONFIGURATION_URL     = 'http://vajonam.github.io/weather-my-way/config/';
+var CONFIGURATION_URL     = 'http://vajonam.github.io/mweather/config/';
 
 var Global = {
   externalDebug:     false, // POST logs to external server - dangerous! lat lon recorded
